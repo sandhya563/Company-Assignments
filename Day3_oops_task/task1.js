537b4fd44673fbbe380206c37d69a196b502ec27
sandhya563 <sandhyas19@navgurukul.org>
2022-08-02 19:17:25 +0530

    update task1

--- Day3_oops_task/task1.js
+++ Day3_oops_task/task1.js
@@ -4,72 +4,70 @@ class Person {
     this.name = name;
   }
 }
-// Class for MP
-class MP extends Person {
-  name;
-  spending_limit;
-  driverName;
-  constituency;
 
-  constructor(name, driverName, constituency, spending_limit) {
+class MP extends Person {
+  constructor(name, driverName, constituency, spending_limit,designation) {
     super(name);
     this.spending_limit = spending_limit;
     this.driverName = driverName;
     this.constituency = constituency;
+    this.designation = designation
   }
+
   getConstituency() {
     console.log("MP Constituency name:", this.constituency);
   }
+
   getDriver() {
     console.log("MP Driver's name:", this.driverName);
   }
-}
-// Class for PM
-class PM extends MP {
-  constructor(name, driverName, constituency, spending_limit) {
-    super(name, driverName, constituency, spending_limit);
-  }
-}
 
-// Class for Minister
-class Minister extends PM {
-  constructor(name, driverName, constituency, spending_limit) {
-    super(name, driverName, constituency, spending_limit);
-    this.can_arrest = false;
-  }
-  check() {
-    if (this.spending_limit > 100000) { // 1 lakh for MP
-      console.log("MP exceeded spending limit");
-      this.can_arrest = true;
-      console.log("Can be arrested by comissioner?:");
-    } else if (this.spending_limit > 1000000) { // 10 lakh for PM
-      console.log("PM exceeded spending limit");
-      this.can_arrest = true;
-      console.log("Can be arrested by comissioner?:");
-    } else if (this.spending_limit > 10000000) { // 1 Crore fro minister
-      console.log("minister exceeded spending limit");
-      this.can_arrest = true;
-      console.log("Can be arrested by comissioner?");
-    } else {
-      console.log("Nobody exceeded spending limit");
-      console.log("MP spending:", this.spending_limit);
-      console.log("PM spending:", this.spending_limit);
-      console.log("Minister spending:", this.spending_limit);
+  check(obj, amount) {
+    if (obj.spending_limit >= amount && obj.designation === "MP") {
+      this.can_arrest = false;
+      console.log("Can't be arrested MP");
+    } else if(obj.spending_limit >= amount && obj.designation === "PM"){
+        this.can_arrest = false;
+      console.log("Can't be arrested PM");
+    }else if(obj.spending_limit >= amount && obj.designation === "Minister"){
+        this.can_arrest = false;
+        console.log("Can't be arrested Minister");
+    }else{
+        this.can_arrest = true;
+        console.log("Can be arrested by comissioner");
     }
   }
 }
 
+// Class for PM
+class PM extends MP {
+  constructor(name, driverName, constituency, spending_limit, designation) {
+    super(name, driverName, constituency,spending_limit,designation);
+    this.spending_limit = spending_limit;
+    this.designation = designation;
+  }
+}
+
+
+// Class for Minister
+class Minister extends PM {
+  constructor(name, driverName, constituency, spending_limit,designation) {
+    super(name, driverName, constituency, spending_limit,designation);
+    this.can_arrest = false;
+    this.designation = designation;
+
+  }
+}
+
 const personObj = new Person("Sandhya");
-console.log(personObj);
 
-const mpObj = new MP("sonu", "andy", "Agra", 100000);
-mpObj.getConstituency();
-mpObj.getDriver();
-// console.log(mpObj);
+const mpObj = new MP("sonu", "andy", "Agra", 100000, "MP");
+//   console.log(mpObj);
 
-const pmObj = new PM("sarika", "tom", "Mainpuri", 1000000);
-// console.log(pmObj);
+const pmObj = new PM("sarika", "tom", "Mainpuri", 1000000, "PM");
+  console.log(pmObj);
 
-const ministerObj = new Minister("Rahul", "sanju", "Delhi", 10000000);
-ministerObj.check();
-// console.log(ministerObj);
+const ministerObj = new Minister("Rahul", "sanju", "Delhi", 10000000, "Minister");
+mpObj.check(pmObj, 1000000);
+//   console.log(ministerObj);
+
